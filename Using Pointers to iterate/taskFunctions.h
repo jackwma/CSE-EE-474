00395226900fedd32867ed6d8f57ac6f30a10abc
jackwma <jackwma@uw.edu>
2019-04-26 00:54:39 -0700

    pointers iteration

---
+++ Using Pointers to iterate/taskFunctions.h
@@ -0,0 +1,12 @@
+#ifndef TASK_FUNCTIONS_H
+#define TASK_FUNCTIONS_H
+
+void measureTask(void*);
+void computeTask(void*);
+void displayTask(void*);
+void setup(void);
+void warningAlarmTask(void*);
+void statusTask(void*);
+void schedulerTask(void*);
+
+#endif
