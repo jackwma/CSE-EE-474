7b3350a3acf7c058fcaa5680a77c9b723a35361c
Junhong Cai <pycaijjh@uw.edu>
2019-04-25 19:05:42 -0700

    aaa

--- main.c
+++ main.c
@@ -1,9 +1,85 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <task.h>
+
+unsigned int temperatureRaw = 75;
+unsigned int systolicPressRaw = 80;
+unsigned int diastolicPressRaw = 80;
+unsigned int pulseRateRaw = 50;
+long mCount = 0;
+int sysComp = 1;
+int measComp = 1;
+int pComp = 1;
 
 
+void measureTask(void* measureData) {
+    struct measureStruct dataMeasure = {&temperatureRaw, &systolicPressRaw, &diastolicPressRaw, &pulseRateRaw};
+    struct measureStruct *measurePtr = &dataMeasure;
+    if (mCount%2 == 0){
+        if (measComp) {
+            *(dataMeasure.temperatureRawPtr) +=2;
+        }else {
+            *(dataMeasure.temperatureRawPtr) -= 2;
+        }
+    } else {
+        if (measComp) {
+            *(dataMeasure.temperatureRawPtr) +=1;
+        }else {
+            *(dataMeasure.temperatureRawPtr) -=1;
+        }
+    }
+    if (temperatureRaw > 50) {
+        measComp = 0;
+    } else if (temperatureRaw < 15){
+        measComp = 1;
+    }
 
+    if (!sysComp) {
+        if(mCount%2 ==0) {
+            *(dataMeasure.systolicPressRaw) +=3;
+        } else {
+            *(dataMeasure.systolicPressRaw) -=1;
+        }
+        if(systolicPressRaw > 100) {
+            sysComp = 1;
+            *(dataMeasure.systolicPressRaw) = 80;
+        }
+    } else {
+        if(mCount%2 ==0) {
+            *(dataMeasure.diastolicPressRaw) +=2;
+        } else {
+            *(dataMeasure.diastolicPressRaw) -=1;
+        }
+        if(diastolicPressRaw < 40) {
+            sysComp = 1;
+            *(dataMeasure.diastolicPressRaw) = 80;
+        }
+    }
+
+    if (mCount%2 == 0){
+        if (pComp) {
+            *(dataMeasure.pulseRateRawPtr) -=1;
+        }else {
+            *(dataMeasure.pulseRateRawPtr) +=1;
+        }
+    } else {
+        if (pComp) {
+            *(dataMeasure.pulseRateRawPtr) +=3;
+        }else {
+            *(dataMeasure.pulseRateRawPtr) -=3;
+        }
+    }
+    if (pulseRateRaw > 40) {
+        measComp = 0;
+    } else if (temperatureRaw < 15){
+        measComp = 1;
+    }
+    mCount+=1;
+}
+
+
+        
 struct computeStruct{
     unsigned int* temperatureRawPtr;
     unsigned int* systolicPressRawPtr;
